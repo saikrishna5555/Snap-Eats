12821efcfe7de308888fc9d93143d06382c67a1b
saikrishna5555 <20mh1a04b5@acoe.edu.in>
2024-04-10 22:28:56 +0530

    dm

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 
 let cart_items=document.querySelector('.cart_items');
 var items_count=0;
+
+add_to_cart=document.querySelector('.add_to_cart');
+add_to_cart.addEventListener('click',function(){
+    items_count+=1;
+    cart_items.textContent=items_count;
+});
